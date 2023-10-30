da7c0290adfd6f76a6dfee2a94b0a646c4dbf1f1
Daniel Contreras <contreras.calderon@gmail.com>
2023-10-30 17:33:27 -0600

    ventas x dia por tipo flete

--- js/utilerias.js
+++ js/utilerias.js
@@ -447,6 +447,20 @@ Date.prototype.getWeek = function() {
     return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
                           - 3 + (week1.getDay() + 6) % 7) / 7);
   }
+
+
+Date.prototype.getWeekDate = function() {
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate()  - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    return date;
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                          - 3 + (week1.getDay() + 6) % 7) / 7);
+  }
   
   // Returns the four-digit year corresponding to the ISO week of the date.
   Date.prototype.getWeekYear = function() {
